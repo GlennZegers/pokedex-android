5be9beed9b72a749452116ecfaf361252b2e7328
Olav Gerritsen <olavjens@hotmail.com>
2020-03-25 13:04:41 +0100

    remove imports

--- app/src/main/java/com/example/pokedex/SettingsActivity.java
+++ app/src/main/java/com/example/pokedex/SettingsActivity.java
@@ -1,26 +1,9 @@
 package com.example.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
-import android.preference.MultiSelectListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceGroup;
-import android.preference.PreferenceManager;
-import android.preference.SwitchPreference;
-import android.util.Log;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.Toast;
 
-import java.util.Collections;
-import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity {
 
